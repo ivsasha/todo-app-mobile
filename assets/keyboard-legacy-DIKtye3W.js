b9a82deaf04fef5d896aa83703bc404c95d6023a
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-09-19 01:00:48 +0300

    Updates

--- assets/keyboard-legacy-DIKtye3W.js
+++ assets/keyboard-legacy-DIKtye3W.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BNNJBepK.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-D53VsOmC.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
